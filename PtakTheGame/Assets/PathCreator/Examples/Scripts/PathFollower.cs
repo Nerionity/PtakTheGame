e5efb119da2a72b75a7f3451b8fb80e4ba10fac4
Nerionity <nerionity@gmail.com>
2022-12-03 08:12:47 +0100

    Ogromna aktualizacja

--- PtakTheGame/Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ PtakTheGame/Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 namespace PathCreation.Examples
 {
@@ -6,10 +7,23 @@ namespace PathCreation.Examples
     // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
     public class PathFollower : MonoBehaviour
     {
+        public playerMOve player;
         public PathCreator pathCreator;
+        public PathCreator pathCreator2;
         public EndOfPathInstruction endOfPathInstruction;
         public float speed = 5;
         float distanceTravelled;
+        Quaternion savedRotation;
+
+        private Coroutine backToFlyCoroutine;
+
+
+        IEnumerator ZmienNaFlyPoCzasie(float time)
+        {
+            yield return new WaitForSeconds(time);
+        
+           GlobalVariables.Instance.playerState = "Fly";
+        }
 
         void Start() {
             if (pathCreator != null)
@@ -19,14 +33,49 @@ namespace PathCreation.Examples
             }
         }
 
+
+        
         void Update()
         {
-            if (pathCreator != null && GlobalVariables.Instance.playerState == "Wind")
+            if (pathCreator != null && GlobalVariables.Instance.playerState == "FirstWind"){
+                if(backToFlyCoroutine != null){
+                    StopCoroutine(backToFlyCoroutine);
+                    backToFlyCoroutine = null;
+                }
+                
+                distanceTravelled=pathCreator.path.GetClosestDistanceAlongPath(gameObject.transform.position);
+                GlobalVariables.Instance.playerState = "Wind";
+            }   
+            if (pathCreator != null && GlobalVariables.Instance.playerState == "Wind" )
             {
                 distanceTravelled += speed * Time.deltaTime;
                 transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
                 transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                if(pathCreator.path.GetClosestTimeOnPath(pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction)) == 1){
+                    //player.FirstFly(pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction));
+                        GlobalVariables.Instance.playerState = "FirstFly";
+                        distanceTravelled = 0;
+                        savedRotation = transform.rotation;
+                }
             }
+            if (pathCreator2 != null &&  GlobalVariables.Instance.playerState == "FirstFly")
+            {
+                distanceTravelled += speed * Time.deltaTime;
+                transform.position = pathCreator2.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+               // transform.rotation = Quaternion.Lerp( Quaternion.Euler(new Vector3(savedRotation.x,savedRotation.y,savedRotation.z)), Quaternion.Euler(new Vector3(savedRotation.x,savedRotation.y,savedRotation.z)), distanceTravelled);
+               
+                //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(savedRotation.x, savedRotation.y, 0), speed * Time.deltaTime);
+                transform.rotation = savedRotation;
+                if(pathCreator2.path.GetClosestTimeOnPath(pathCreator2.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction)) == 1){
+                    //player.FirstFly(pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction));
+                         GlobalVariables.Instance.playerState = "Fly";
+                         player.move.x = transform.rotation.x;
+                         player.move.y = transform.rotation.y;
+
+                        //backToFlyCoroutine = StartCoroutine(ZmienNaFlyPoCzasie(2));
+                }
+            }
+            
         }
 
         // If the path changes during the game, update the distance travelled so that the follower's position on the new path
--- PtakTheGame/Assets/michal/playerMOve.cs
+++ PtakTheGame/Assets/michal/playerMOve.cs
@@ -5,6 +5,7 @@ public class playerMOve : MonoBehaviour
 {
     public GameObject player;
     public Vector3 move;
+    public float secondFlySpeed;
     public float speed = 10;
     public Rigidbody rb;
     private float playerRotationX;
@@ -20,8 +21,8 @@ public class playerMOve : MonoBehaviour
     void Update()
     {   
         if(GlobalVariables.Instance.playerState == "Fly"){
-            move.x += Input.GetAxis("Mouse X");
-            move.y += Input.GetAxis("Mouse Y");
+                move.x += Input.GetAxis("Mouse X");
+                move.y += Input.GetAxis("Mouse Y");
         
             playerRotationX = this.transform.localRotation.eulerAngles.x;
             if (playerRotationX > 180f)
@@ -31,24 +32,30 @@ public class playerMOve : MonoBehaviour
 
             
 
-        if (flying == false)
-        {
-            rb.useGravity = true;
-            speed = 0;
-            transform.localRotation = Quaternion.Euler(0, move.x, 0);
+            if (flying == false)
+            {
+                rb.useGravity = true;
+                speed = 0;
+                transform.localRotation = Quaternion.Euler(0, move.x, 0);
+            }
             
-        }
-        
 
-        if (flying == true)
-        {
-            transform.localRotation = Quaternion.Euler(move.y, move.x, 0);
-            if (playerRotationX < 0)
-            { speed = 10 + (playerRotationX / 9); }
-            else { speed = 10 + (playerRotationX / 9); }
-            rb.velocity = transform.forward * speed;
+            if (flying == true)
+            {
+                transform.localRotation = Quaternion.Euler(move.y, move.x, 0);
+                if (playerRotationX < 0)
+                { speed = 10 + (playerRotationX / 9); }
+                else { speed = 10 + (playerRotationX / 9); }
+
+                if(GlobalVariables.Instance.playerState == "SecondFly"){
+                    rb.velocity = transform.forward * secondFlySpeed;
+                } else {
+                    rb.velocity = transform.forward * speed;
+                }
+            }
         }
-        }
+
+        Debug.Log(rb.velocity.magnitude);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -59,5 +66,18 @@ public class playerMOve : MonoBehaviour
         }
     }
 
+    // public void DisableCollider(){
+    //     GetComponent<BoxCollider>().enabled = false;
+    // }
 
+    // public void FirstFly(Quaternion dir){
+    //     //transform.localPosition = transform.localPosition + transform.forward * 2f;
+    //     move = dir.eulerAngles;
+    //     //StartCoroutine(ReenableCollider());
+    // }
+
+    // private IEnumerator ReenableCollider(){
+    //     yield return new WaitForSeconds(1f);
+    //     GetComponent<BoxCollider>().enabled = true;
+    // }
 }
