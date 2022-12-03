452e8c7927c305bcbf2d2082245f5c52930b98d5
MichalBoMoge <MichalBoMoge@wp.pl>
2022-12-03 10:26:24 +0100

    Revert "Merge branch 'main' of https://github.com/Nerionity/PtakTheGame"
    This reverts commit fba933e44d8616c4a583e3ecbaf723ea6c34ac88.

--- PtakTheGame/Assets/michal/playerMOve.cs
+++ PtakTheGame/Assets/michal/playerMOve.cs
@@ -1,24 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
 public class playerMOve : MonoBehaviour
 {
+    
+    public GameObject border;
+    public TMP_Text zebrane;
+    public GameObject cameraBlock;
+    public GameObject kurnik1,kurnik2,kurnik3;
+    public Vector3 Offset = new Vector3(0, 10, 0);
+    public TMP_Text text;
     public GameObject player;
     public Vector3 move;
     public float secondFlySpeed;
     public float speed = 10;
     public Rigidbody rb;
     private float playerRotationX;
-    public bool flying;
+    public float force = 500;
+    private bool zasieg;
+    private float KtoryKurnik;
+    private bool zebrane1,zebrane2,zebrane3;
+    
     void Start()
     {
-        flying = true;
+        
+        zebrane.enabled = false;
+        cameraBlock.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
+        text.enabled = false;
+        rb.useGravity = false;
+        
+    }
+
+
+    private IEnumerator wait(GameObject g, float w)
+    {
+        rb.isKinematic = true;
+        text.enabled = false;
+        zebrane.text = ("zebrales dane z kurnika nr "+ KtoryKurnik.ToString());
+        zebrane.enabled = true;
+        cameraBlock.SetActive(true);
+        Destroy(g);       
+        yield return new WaitForSeconds(1);
+        zebrane.enabled = false ;
+        rb.isKinematic=false;
+        cameraBlock.SetActive(false);
+        zasieg = false;
+        
     }
 
     // Update is called once per frame
+
+
+     
     void Update()
+<<<<<<< HEAD
+    {
+
+        if(Input.GetKey(KeyCode.Escape))Application.Quit();
+
+
+
+
+        if (GlobalVariables.Instance.playerState == "Fly")
+        {
+            move.x += Input.GetAxis("Mouse X");
+            move.y += Input.GetAxis("Mouse Y");
+
+
+            if (rb.useGravity == false)
+=======
     {   
         if(GlobalVariables.Instance.playerState == "Fly"){
                 move.x += Input.GetAxis("Mouse X");
@@ -26,12 +83,53 @@ public class playerMOve : MonoBehaviour
         
             playerRotationX = this.transform.localRotation.eulerAngles.x;
             if (playerRotationX > 180f)
+>>>>>>> 75599e7f8f5012bfe4ad0ccdebdff0c95bb6c3fc
             {
-                playerRotationX = playerRotationX - 360f;
+                playerRotationX = this.transform.localRotation.eulerAngles.x;
+                if (playerRotationX > 180f)
+                {
+                    playerRotationX = playerRotationX - 360f;
+
+                }
+                transform.localRotation = Quaternion.Euler(move.y, move.x, 0);
+                if (playerRotationX < 0)
+                { speed = 10 + (playerRotationX / 9); }
+                else
+                {
+                    speed = 10 + (playerRotationX / 9);
+
+                }
+                rb.velocity = transform.forward * speed;
             }
+                if (zasieg == true)
+                {
+
+
+                    if (Input.GetKeyDown(KeyCode.F))
+                    {
+                        switch (KtoryKurnik)
+                        {
+                            case 1: StartCoroutine(wait(kurnik1, KtoryKurnik)); zebrane1 = true; break;
+                            case 2: StartCoroutine(wait(kurnik2, KtoryKurnik)); zebrane2 = true; break;
+                            case 3: StartCoroutine(wait(kurnik3, KtoryKurnik)); zebrane3 = true; break;
+                        }
+
+
+
+                    }
+                }
+
+                if (zebrane1 == true && zebrane2 == true && zebrane3 == true)
+                {
+                    zebrane.text = "ZEBRALES WSZYSTKO TERAZ UCIEKAJ";
+                    zebrane.enabled = true;
+                    border.SetActive(false);
+                }
 
             
 
+<<<<<<< HEAD
+=======
             if (flying == false)
             {
                 rb.useGravity = true;
@@ -53,18 +151,59 @@ public class playerMOve : MonoBehaviour
                     rb.velocity = transform.forward * speed;
                 }
             }
+>>>>>>> 75599e7f8f5012bfe4ad0ccdebdff0c95bb6c3fc
         }
 
         Debug.Log(rb.velocity.magnitude);
     }
 
-    private void OnCollisionEnter(Collision collision)
+ 
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "terrain")
+        if(other.gameObject.tag =="karmnik")
         {
-            flying = false;
+            zasieg = true;
+            KtoryKurnik = 1;
+            text.enabled = true;
+        }
+        if (other.gameObject.tag == "karmnik2")
+        {
+            zasieg = true;
+            KtoryKurnik = 2;
+            text.enabled = true;
+        }
+        if (other.gameObject.tag == "karmnik3")
+        {
+            zasieg = true;
+            KtoryKurnik = 3;
+            text.enabled = true;
+        }
+
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "karmnik" || other.gameObject.tag == "karmnik2" || other.gameObject.tag == "karmnik3")
+        {
+            zasieg = false;
+            text.enabled = false;
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+        if (collision.gameObject.tag != "border")
+        {
+            rb.useGravity = true;
+            zebrane.text = "MISSION FAILED! PRESS ESC TO QUIT";
+            zebrane.enabled = true;
+        }
+          
+
+    }
+<<<<<<< HEAD
+=======
 
     // public void DisableCollider(){
     //     GetComponent<BoxCollider>().enabled = false;
@@ -80,4 +219,5 @@ public class playerMOve : MonoBehaviour
     //     yield return new WaitForSeconds(1f);
     //     GetComponent<BoxCollider>().enabled = true;
     // }
+>>>>>>> 75599e7f8f5012bfe4ad0ccdebdff0c95bb6c3fc
 }
